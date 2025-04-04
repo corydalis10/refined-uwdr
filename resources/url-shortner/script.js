a2ac69aef795ec2ac2a9a3e3c9a3e27185e968a9
Hyeonho Kang <hyeonhokang10@kakao.com>
2025-04-04 17:15:27 +0900

    Delete unnecessary resources

--- resources/url-shortner/script.js
+++
@@ -1,8 +0,0 @@
-const originalUrlInput = document.getElementById("originalUrl");
-const shortenButton = document.getElementById("shortenButton");
-const shortenedUrlParagraph = document.getElementById("shortenedUrl");
-shortenButton.addEventListener("click", () => {
-    const originalUrl = originalUrlInput.value;
-    const shortenedUrl = originalUrl;
-    shortenedUrlParagraph.textContent = shortenedUrl;
-});
